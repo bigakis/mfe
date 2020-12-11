09fb86f8afc5836474e5c906e8d60d8a739ce022
bigakis <bigakisgr@yahoo.com>
2020-12-11 13:40:39 +0200

    added auth subapp

--- packages/container/src/components/AuthApp.js
+++ packages/container/src/components/AuthApp.js
@@ -1,10 +1,10 @@
 import React, { useRef, useEffect} from 'react';
 
-import { mount } from 'marketing/MarketingApp';
+import { mount } from 'auth/AuthApp';
 import { useHistory } from 'react-router-dom';
 
 
-export default () => {
+export default ({onSignIn}) => {
   const ref = useRef(null);
   const history = useHistory();
 
@@ -17,7 +17,8 @@ export default () => {
         if (pathname !== nextPathname) {
           history.push(nextPathname);
         }
-      }
+      },
+      onSignIn
     });
     history.listen(onParentNavigate);
   }, []);
